38eccfa3737d0a6544fbb06d5b01010d23b6b5ed
Ildar <ildar-t3p@yandex.ru>
2021-05-20 20:27:18 +0300

    start main page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-main-wrapper/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+    import vMainWrapper from './components/v-main-wrapper'
+
+    export default {
+        name: 'App',
+        components: {
+            vMainWrapper,
+        },
+        computed: {},
+        methods: {},
+        watch: {},
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -1,13 +1,23 @@
 <template>
-    $END$
+    <div class="v-main-wrapper">
+        Привет
+    </div>
 </template>
 
 <script>
+
+
     export default {
-        name: "v-main-wrapper"
+        name: "v-main-wrapper",
+        props: {},
+        data() {
+            return {
+                title: 'Main hi',
+            }
+        },
     }
 </script>
 
-<style scoped>
+<style>
 
 </style>
