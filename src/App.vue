3708dd737d6f8bb71079b875e298e2f294dd666d
Ildar <ildar-t3p@yandex.ru>
2021-05-25 07:40:49 +0300

    add fonts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <v-main-wrapper/>
-    привет
 </template>
 
 <script>
@@ -20,12 +19,4 @@
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles/style.css'
+import './assets/js/rem.js'
+import './assets/styles/fonts.css'
 
 
 createApp(App).mount('#app')
